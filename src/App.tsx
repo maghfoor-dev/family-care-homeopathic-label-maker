a154fec8120114b347e728eed1ecdf5a2c259869
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2024-02-19 08:51:22 +0000

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import {
+  NavigationMenu,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "./components/ui/navigation-menu";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -11,7 +17,19 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col max-w-7xl mx-auto">
+      <NavigationMenu className="justify-between p-4 max-w-7xl">
+        <p>FAMILY CARE</p>
+        <NavigationMenuLink
+          className={`${navigationMenuTriggerStyle()} underline cursor-pointer bg-transparent`}
+        >
+          Medicine List
+        </NavigationMenuLink>
+        <Button className="bg-[#FFE882] text-black hover:bg-[#FFE882] hover:opacity-80">
+          Print Queue
+        </Button>
+      </NavigationMenu>
+
       <h1>Welcome to Tauri!</h1>
 
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
