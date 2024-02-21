7f9ea3bea707e58ee097435b97578e1e3178eca7
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2024-02-21 20:18:02 +0000

    created two boxes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import {
   navigationMenuTriggerStyle,
 } from "./components/ui/navigation-menu";
 import { Button } from "./components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
+import { Textarea } from "./components/ui/textarea";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -17,7 +19,7 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col max-w-7xl mx-auto">
+    <div className="flex flex-col max-w-7xl mx-auto gap-9">
       <NavigationMenu className="justify-between p-4 max-w-7xl">
         <p>FAMILY CARE</p>
         <NavigationMenuLink
@@ -30,7 +32,7 @@ function App() {
         </Button>
       </NavigationMenu>
 
-      <h1>Welcome to Tauri!</h1>
+      {/* <h1>Welcome to Tauri!</h1>
 
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
@@ -49,7 +51,35 @@ function App() {
         <button type="submit">Greet</button>
       </form>
 
-      <p>{greetMsg}</p>
+      <p>{greetMsg}</p> */}
+
+      <section className="flex flex-col md:flex-row justify-center items-center gap-6">
+        <div className="flex flex-col justify-center gap-4">
+          <Card className="min-card-width min-card-height">
+            <CardHeader>
+              <CardTitle>SKU Codes (0)</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Textarea
+                className="h-[400px]"
+                placeholder="Add your SKU codes here..."
+              ></Textarea>
+            </CardContent>
+          </Card>
+          <Button variant={"secondary"}>Search Codes</Button>
+        </div>
+        <div className="flex flex-col justify-center gap-4">
+          <Card className="min-card-width min-card-height">
+            <CardHeader>
+              <CardTitle>Found Codes (0)</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="bg-background h-[400px] py-2 px-3 rounded-md"></div>
+            </CardContent>
+          </Card>
+          <Button variant={"secondary"}>Add to Queue</Button>
+        </div>
+      </section>
     </div>
   );
 }
