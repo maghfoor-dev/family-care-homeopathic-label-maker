e517765d9560493a505b2b329c1b1efd7fb4da1f
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2024-02-23 18:56:24 +0000

    handling adding and searching codes

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,30 @@ const SKU_IDS = [
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
-  const [searchedCodesLen, setSearchedCodesLen] = useState(0);
+  const [addedCodes, setAddedCodes] = useState<string[]>([]);
+  const [foundCodes, setFoundCodes] = useState<string[]>([
+    "code-1",
+    "code-2",
+    "code-3",
+    "code-4",
+    "code-5",
+    "code-6",
+    "code-7",
+    "code-1",
+    "code-2",
+    "code-3",
+    "code-4",
+    "code-5",
+    "code-6",
+    "code-7",
+    "code-1",
+    "code-2",
+    "code-3",
+    "code-4",
+    "code-5",
+    "code-6",
+    "code-7",
+  ]);
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -30,9 +53,34 @@ function App() {
   }
 
   function createCodesArr(input: string) {
-    const finalStrArr = input.split("\n");
-    console.log(finalStrArr, "IS THE FINAL ARR");
-    setSearchedCodesLen(finalStrArr.length);
+    // splitting codes by lines
+    const inputCodes: string[] = input.split("\n");
+
+    // removing any spaces from the codes
+    const removedSpacesCodes: string[] = inputCodes.map((code) =>
+      code.replace(/\s/g, "")
+    );
+
+    // removing any empty strings
+    const finalCodes = removedSpacesCodes.filter((code) => code.length > 0);
+
+    //settings codes to states
+    setAddedCodes(finalCodes);
+  }
+
+  function handleFindCodes() {
+    const foundCodesArr: string[] = [];
+
+    for (const code of addedCodes) {
+      console.log(code);
+      if (SKU_IDS.includes(code)) {
+        foundCodesArr.push(code);
+      }
+    }
+
+    console.log(foundCodes, "ARE THE FOUND CODES");
+
+    setFoundCodes(foundCodesArr);
   }
 
   return (
@@ -74,7 +122,7 @@ function App() {
         <div className="flex flex-col justify-center gap-4">
           <Card className="min-card-width min-card-height">
             <CardHeader>
-              <CardTitle>SKU Codes ({searchedCodesLen})</CardTitle>
+              <CardTitle>SKU Codes ({addedCodes.length})</CardTitle>
             </CardHeader>
             <CardContent>
               <Textarea
@@ -84,20 +132,31 @@ function App() {
               ></Textarea>
             </CardContent>
           </Card>
-          <Button variant={"secondary"}>Search Codes</Button>
+          <Button variant={"secondary"} onClick={handleFindCodes}>
+            Search Codes
+          </Button>
         </div>
         <div className="flex flex-col justify-center gap-4">
           <Card className="min-card-width min-card-height">
             <CardHeader>
-              <CardTitle>Found Codes (0)</CardTitle>
+              <CardTitle>Found Codes ({foundCodes.length})</CardTitle>
             </CardHeader>
             <CardContent>
-              <div className="bg-gray-300 h-[400px] py-2 px-3 rounded-md"></div>
+              <div className="bg-gray-300 h-[400px] py-2 px-3 rounded-md flex flex-col gap-1 overflow-scroll overflow-x-hidden">
+                {foundCodes.map((foundCode) => {
+                  return (
+                    <Card className="bg-[#F2F5DE] p-2 text-sm">
+                      <div>{foundCode}</div>
+                    </Card>
+                  );
+                })}
+              </div>
             </CardContent>
           </Card>
           <Button variant={"secondary"}>Add to Queue</Button>
         </div>
       </section>
+      <div>{JSON.stringify(addedCodes, null, 0)}</div>
     </div>
   );
 }
