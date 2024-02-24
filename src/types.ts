9220ae0746892543e7b9cf70b0efaf81e13bfe33
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2024-02-24 09:28:09 +0000

    created a type for the medicine

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type MedicineType = {
+  id: string;
+  name: string;
+  potency: string;
+  quantity: string;
+  sticker_name: string;
+  sku_number: string;
+  category: string;
+  stored_location: string;
+};
