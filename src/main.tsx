fc69142d9df95a679382f789efb1d084caca9893
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2024-02-24 10:03:50 +0000

    setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./styles.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MedicineListPage from "./pages/medicine-list.page";
+import HomePage from "./pages/home.page";
+import NavBar from "./components/custom-navbar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/medicine-list",
+    element: <MedicineListPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <main className="flex flex-col max-w-7xl mx-auto gap-9">
+      <NavBar />
+      {/* <App /> */}
+      <RouterProvider router={router} />
+    </main>
+  </React.StrictMode>
 );
