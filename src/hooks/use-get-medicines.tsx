e1223d91e4198076021738f805ed41faabc0cd10
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2024-03-04 20:08:19 +0000

    created hook to get updated medicines

---
+++ src/hooks/use-get-medicines.tsx
@@ -0,0 +1,21 @@
+import { getAllMedicines } from "@/lib/utils";
+import { MedicineType } from "@/types";
+import { useEffect, useState } from "react";
+
+export default function useGetMedicines() {
+    const [medicines, setMedicines] = useState<MedicineType[]>([])
+
+    useEffect(() => {
+        updateMedicines()
+    }, [])
+
+    async function updateMedicines() {
+        const newMedicineData = await getAllMedicines();
+        setMedicines(newMedicineData)
+    }
+
+    return {
+        medicines,
+        updateMedicines
+    }
+}
