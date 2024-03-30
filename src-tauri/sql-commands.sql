622d2ced2fc4b917788c61ad41c3386b00547d28
maghfoor-dev <maghfoor.dev@gmail.com>
2024-03-30 13:04:26 +0000

    added medicine_queue_list table

--- src-tauri/sql-commands.sql
+++ src-tauri/sql-commands.sql
@@ -9,3 +9,9 @@ CREATE TABLE medicine_list (
     stored_location TEXT,
     CONSTRAINT id_length CHECK(length(id) = 6)
 );
+
+CREATE TABLE medicine_queue_list (
+    queue_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    medicine_id INTEGER,
+    FOREIGN KEY (medicine_id) REFERENCES medicine_list(id)
+);
